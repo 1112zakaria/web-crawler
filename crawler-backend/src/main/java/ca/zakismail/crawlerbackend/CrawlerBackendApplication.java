f27b289215c5770f2c810cc3a9a8cd29ccbedd83
Zakaria Ismail <1112zakaria@gmail.com>
2023-10-07 00:21:59 -0400

    Java backend starter project files
    Created using Spring Initializr

---
+++ crawler-backend/src/main/java/ca/zakismail/crawlerbackend/CrawlerBackendApplication.java
@@ -0,0 +1,13 @@
+package ca.zakismail.crawlerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlerBackendApplication.class, args);
+	}
+
+}
---
+++ crawler-backend/src/test/java/ca/zakismail/crawlerbackend/CrawlerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.zakismail.crawlerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
